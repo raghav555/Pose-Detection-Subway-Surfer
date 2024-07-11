2a81508b439f2895a47b3b0f826c1208c75baf5c
Raghav Singh Chauhan <raghav14082@iiitd.ac.in>
2024-07-11 18:20:46 -0400

    Added code, report and video submission for Computer Vision Final Project

---
+++ Code/cvipproject.py
@@ -0,0 +1,380 @@
+import cv2
+import pyautogui
+from time import time
+from math import hypot
+import mediapipe as mp
+import matplotlib.pyplot as plt
+
+# Initialize mediapipe pose class.
+mp_pose = mp.solutions.pose
+
+# Setup the Pose function for images.
+pose_image = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5, model_complexity=1)
+
+# Setup the Pose function for videos.
+pose_video = mp_pose.Pose(static_image_mode=False, model_complexity=1, min_detection_confidence=0.7,
+                          min_tracking_confidence=0.7)
+
+# Initialize mediapipe drawing class.
+mp_drawing = mp.solutions.drawing_utils 
+
+def detectPose(image, pose, draw=False, display=False):
+
+    
+    # Create a copy of the input image.
+    output_image = image.copy()
+    
+    # Convert the image from BGR into RGB format.
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # Perform the Pose Detection.
+    results = pose.process(image_rgb)
+    
+    # Check if any landmarks are detected and are specified to be drawn.
+    if results.pose_landmarks and draw:
+    
+        # Draw Pose Landmarks on the output image.
+        mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks,
+                                  connections=mp_pose.POSE_CONNECTIONS,
+                                  landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255,255,255),
+                                                                               thickness=3, circle_radius=3),
+                                  connection_drawing_spec=mp_drawing.DrawingSpec(color=(49,125,237),
+                                                                               thickness=2, circle_radius=2))
+
+    # Check if the original input image and the resultant image are specified to be displayed.
+    if display:
+    
+        # Display the original input image and the resultant image.
+        plt.figure(figsize=[22,22])
+        plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
+        plt.subplot(122);plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+        
+    # Otherwise
+    else:
+
+        # Return the output image and the results of pose landmarks detection.
+        return output_image, results
+
+def checkHandsJoined(image, results, draw=False, display=False):
+
+    
+    # Get the height and width of the input image.
+    height, width, _ = image.shape
+    
+    # Create a copy of the input image to write the hands status label on.
+    output_image = image.copy()
+    
+    # Get the left wrist landmark x and y coordinates.
+    left_wrist_landmark = (results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x * width,
+                          results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y * height)
+
+    # Get the right wrist landmark x and y coordinates.
+    right_wrist_landmark = (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x * width,
+                           results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y * height)
+    
+    # Calculate the euclidean distance between the left and right wrist.
+    euclidean_distance = int(hypot(left_wrist_landmark[0] - right_wrist_landmark[0],
+                                   left_wrist_landmark[1] - right_wrist_landmark[1]))
+    
+    # Compare the distance between the wrists with a appropriate threshold to check if both hands are joined.
+    if euclidean_distance < 130:
+        
+        # Set the hands status to joined.
+        hand_status = 'Hands Joined'
+        
+        # Set the color value to green.
+        color = (0, 255, 0)
+        
+    # Otherwise.    
+    else:
+        
+        # Set the hands status to not joined.
+        hand_status = 'Hands Not Joined'
+        
+        # Set the color value to red.
+        color = (0, 0, 255)
+        
+    # Check if the Hands Joined status and hands distance are specified to be written on the output image.
+    if draw:
+
+        # Write the classified hands status on the image. 
+        cv2.putText(output_image, hand_status, (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, color, 3)
+        
+        # Write the the distance between the wrists on the image. 
+        cv2.putText(output_image, f'Distance: {euclidean_distance}', (10, 70),
+                    cv2.FONT_HERSHEY_PLAIN, 2, color, 3)
+        
+    # Check if the output image is specified to be displayed.
+    if display:
+
+        # Display the output image.
+        plt.figure(figsize=[10,10])
+        plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+    
+    # Otherwise
+    else:
+    
+        # Return the output image and the classified hands status indicating whether the hands are joined or not.
+        return output_image, hand_status
+
+def checkLeftRight(image, results, draw=False, display=False):
+
+    # Declare a variable to store the horizontal position (left, center, right) of the person.
+    horizontal_position = None
+    
+    # Get the height and width of the image.
+    height, width, _ = image.shape
+    
+    # Create a copy of the input image to write the horizontal position on.
+    output_image = image.copy()
+    
+    # Retreive the x-coordinate of the left shoulder landmark.
+    left_x = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x * width)
+
+    # Retreive the x-corrdinate of the right shoulder landmark.
+    right_x = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x * width)
+    
+    # Check if the person is at left that is when both shoulder landmarks x-corrdinates
+    # are less than or equal to the x-corrdinate of the center of the image.
+    if (right_x <= width//2 and left_x <= width//2):
+        
+        # Set the person's position to left.
+        horizontal_position = 'Left'
+
+    # Check if the person is at right that is when both shoulder landmarks x-corrdinates
+    # are greater than or equal to the x-corrdinate of the center of the image.
+    elif (right_x >= width//2 and left_x >= width//2):
+        
+        # Set the person's position to right.
+        horizontal_position = 'Right'
+    
+    # Check if the person is at center that is when right shoulder landmark x-corrdinate is greater than or equal to
+    # and left shoulder landmark x-corrdinate is less than or equal to the x-corrdinate of the center of the image.
+    elif (right_x >= width//2 and left_x <= width//2):
+        
+        # Set the person's position to center.
+        horizontal_position = 'Center'
+        
+    # Check if the person's horizontal position and a line at the center of the image is specified to be drawn.
+    if draw:
+
+        # Write the horizontal position of the person on the image. 
+        cv2.putText(output_image, horizontal_position, (5, height - 10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+        
+        # Draw a line at the center of the image.
+        cv2.line(output_image, (width//2, 0), (width//2, height), (255, 255, 255), 2)
+        
+    # Check if the output image is specified to be displayed.
+    if display:
+
+        # Display the output image.
+        plt.figure(figsize=[10,10])
+        plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+    
+    # Otherwise
+    else:
+    
+        # Return the output image and the person's horizontal position.
+        return output_image, horizontal_position
+
+def checkJumpCrouch(image, results, MID_Y=300, draw=False, display=False):
+
+    # Get the height and width of the image.
+    height, width, _ = image.shape
+    
+    # Create a copy of the input image to write the posture label on.
+    output_image = image.copy()
+    
+    # Retreive the y-coordinate of the left shoulder landmark.
+    left_y = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y * height)
+
+    # Retreive the y-coordinate of the right shoulder landmark.
+    right_y = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y * height)
+
+    # Calculate the y-coordinate of the mid-point of both shoulders.
+    actual_mid_y = abs(right_y + left_y) // 2
+    
+    # Calculate the upper and lower bounds of the threshold.
+    lower_bound = MID_Y-15
+    upper_bound = MID_Y+100
+    
+    # Check if the person has jumped that is when the y-coordinate of the mid-point 
+    # of both shoulders is less than the lower bound.
+    if (actual_mid_y < lower_bound):
+        
+        # Set the posture to jumping.
+        posture = 'Jumping'
+    
+    # Check if the person has crouched that is when the y-coordinate of the mid-point 
+    # of both shoulders is greater than the upper bound.
+    elif (actual_mid_y > upper_bound):
+        
+        # Set the posture to crouching.
+        posture = 'Crouching'
+    
+    # Otherwise the person is standing and the y-coordinate of the mid-point 
+    # of both shoulders is between the upper and lower bounds.    
+    else:
+        
+        # Set the posture to Standing straight.
+        posture = 'Standing'
+        
+    # Check if the posture and a horizontal line at the threshold is specified to be drawn.
+    if draw:
+
+        # Write the posture of the person on the image. 
+        cv2.putText(output_image, posture, (5, height - 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+        
+        # Draw a line at the intial center y-coordinate of the person (threshold).
+        cv2.line(output_image, (0, MID_Y),(width, MID_Y),(255, 255, 255), 2)
+        
+    # Check if the output image is specified to be displayed.
+    if display:
+
+        # Display the output image.
+        plt.figure(figsize=[10,10])
+        plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+    
+    # Otherwise
+    else:
+    
+        # Return the output image and posture indicating whether the person is standing straight or has jumped, or crouched.
+        return output_image, posture
+
+
+
+# Initialize the VideoCapture object to read from the webcam.
+camera_video = cv2.VideoCapture(0)
+camera_video.set(3,1280)
+camera_video.set(4,960)
+
+cv2.namedWindow('Subway surfers playing through pose detection', cv2.WINDOW_NORMAL)
+ 
+time1 = 0
+
+game_started = False   
+
+x_pos_index = 1
+
+y_pos_index = 1
+
+MID_Y = None
+
+counter = 0
+
+num_of_frames = 10
+
+while camera_video.isOpened():
+    
+    ok, frame = camera_video.read()
+    
+    if not ok:
+        continue
+    
+    frame = cv2.flip(frame, 1)
+    
+    frame_height, frame_width, _ = frame.shape
+    
+    frame, results = detectPose(frame, pose_video, draw=game_started)
+    
+    if results.pose_landmarks:
+        
+        if game_started:
+            
+            frame, horizontal_position = checkLeftRight(frame, results, draw=True)
+            
+            if (horizontal_position=='Left' and x_pos_index!=0) or (horizontal_position=='Center' and x_pos_index==2):
+                
+                pyautogui.press('left')
+                
+                x_pos_index -= 1               
+
+            elif (horizontal_position=='Right' and x_pos_index!=2) or (horizontal_position=='Center' and x_pos_index==0):
+                
+                pyautogui.press('right')
+                
+                x_pos_index += 1
+            
+        else:
+            
+            cv2.putText(frame, 'JOIN BOTH HANDS TO START THE GAME.', (5, frame_height - 10), cv2.FONT_HERSHEY_PLAIN,
+                        2, (0, 255, 0), 3)
+        
+        if checkHandsJoined(frame, results)[1] == 'Hands Joined':
+
+            counter += 1
+
+            if counter == num_of_frames:
+
+                if not(game_started):
+
+                    game_started = True
+
+                    left_y = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y * frame_height)
+
+                    right_y = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y * frame_height)
+
+                    MID_Y = abs(right_y + left_y) // 2
+                    
+                    pyautogui.click(x=1300, y=800, button='left')
+                
+                else:
+
+                    # Press the space key.
+                    pyautogui.press('space')
+                
+                #----------------------------------------------------------------------------------------------------------
+                
+                # Update the counter value to zero.
+                counter = 0
+
+        else:
+
+            counter = 0
+            
+        if MID_Y:
+            cv2.putText(frame, 'MID_Y: {}'.format(int(MID_Y)), (50, 30),cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
+            frame, posture = checkJumpCrouch(frame, results, MID_Y, draw=True)
+            
+            if posture == 'Jumping' and y_pos_index == 1:
+
+                pyautogui.press('up')
+                
+                y_pos_index += 1 
+
+            elif posture == 'Crouching' and y_pos_index == 1:
+
+                pyautogui.press('down')
+                
+                y_pos_index -= 1
+            
+            elif posture == 'Standing' and y_pos_index   != 1:
+                
+                y_pos_index = 1
+        
+       
+    
+    else:
+
+        counter = 0
+        
+    time2 = time()    
+      
+    if (time2 - time1) > 0:
+    
+        frames_per_second = 1.0 / (time2 - time1)
+        
+        cv2.putText(frame, 'FPS: {}'.format(int(frames_per_second)), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
+    
+    time1 = time2
+    
+    cv2.imshow('Subway Surfers with Pose Detection', frame)
+      
+    k = cv2.waitKey(1) & 0xFF    
+    
+    if(k == 27):
+        break
+
+camera_video.release()
+cv2.destroyAllWindows()
+     
